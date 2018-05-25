69ad837473e31f3b4e7140db6f9cbb87cabfb9a6
Antal Bos <antal.bos@amplexor.com>
2018-05-25 13:48:00 +0200

    Layout modified to add a better pomodoro timer

--- app/src/main/java/nl/bos/onepomodoroexercises/MainActivity.java
+++ app/src/main/java/nl/bos/onepomodoroexercises/MainActivity.java
@@ -2,20 +2,28 @@ package nl.bos.onepomodoroexercises;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import nl.bos.onepomodoroexercises.models.Exercise;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener, View.OnClickListener, Runnable {
 
     private static final String LOG_TAG = "MainActivity";
     private final List<Exercise> exercises = new ArrayList<>();
     private ExerciseAdapter adapter;
+    private TextView timer;
+    private Button button;
+    private Thread timerThread;
+    private int countDownTimer = 25*60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,12 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         viewExercises.setOnItemClickListener(this);
         viewExercises.setOnItemLongClickListener(this);
 
+        button = findViewById(R.id.btnStart);
+        button.setOnClickListener(this);
+
+        timer = findViewById(R.id.txtTimer);
+        timerThread = new Thread(this);
+
         adapter = new ExerciseAdapter(this);
         viewExercises.setAdapter(adapter);
         new RetrieveDataTask(exercises, adapter, getApplicationContext()).execute("https://drive.google.com/uc?id=1ObJ5KUPpppQWakof-Bym4MEdX_r4LpSB&export=download");
@@ -34,14 +48,34 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        Log.i(LOG_TAG, "click" + id);
+        Log.i(LOG_TAG, "click " + id);
     }
 
     @Override
     public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-        Log.i(LOG_TAG, "long click" + id);
+        Log.i(LOG_TAG, "long click " + id);
         exercises.remove(position);
         adapter.updateResults(exercises);
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i(LOG_TAG, "Start click");
+        timerThread.start();
+        button.setEnabled(false);
+    }
+
+    @Override
+    public void run() {
+        final Handler handler =new Handler(Looper.getMainLooper());
+        final Runnable r = new Runnable() {
+            public void run() {
+                handler.postDelayed(this, 1000);
+                timer.setText(String.format("%02d:%02d", countDownTimer / 60, countDownTimer % 60));
+                countDownTimer--;
+            }
+        };
+        handler.postDelayed(r, 0000);
     }
 }
