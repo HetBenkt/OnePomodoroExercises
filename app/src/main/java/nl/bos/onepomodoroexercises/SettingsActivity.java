36b9feab2262170ff10682c99a1572e924f8a53f
Antal Bos <antal.bos@amplexor.com>
2018-06-05 14:08:09 +0200

    With a working backbutton in the settings

--- app/src/main/java/nl/bos/onepomodoroexercises/SettingsActivity.java
+++ app/src/main/java/nl/bos/onepomodoroexercises/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
     private static final String TAG = SettingsActivity.class.getSimpleName();
@@ -20,5 +21,15 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.settings);
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
