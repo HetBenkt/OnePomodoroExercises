552a8ed78fda4693ae2aae3bba5175d75e43cdfc
Antal Bos <antal.bos@amplexor.com>
2018-07-20 16:30:57 +0200

    Making a button available to update the database. The real update code is in the local_db branch

--- app/src/main/java/nl/bos/onepomodoroexercises/SettingsFragment.java
+++ app/src/main/java/nl/bos/onepomodoroexercises/SettingsFragment.java
@@ -1,15 +1,29 @@
 package nl.bos.onepomodoroexercises;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 @SuppressWarnings("WeakerAccess")
 public class SettingsFragment extends PreferenceFragment {
+    private static final String TAG = SettingsActivity.class.getSimpleName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference updateLocalData = findPreference(getString(R.string.updateLocalData));
+        updateLocalData.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Log.i(TAG, "Update database!");
+                return true;
+            }
+        });
+
     }
 }
