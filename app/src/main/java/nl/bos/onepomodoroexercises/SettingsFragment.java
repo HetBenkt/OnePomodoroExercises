69697c84c5f90425ff38fa1e74d9f6998b24a4cd
Antal Bos <antal.bos@amplexor.com>
2018-08-10 13:11:36 +0200

    Preferences summary color and adjusting textsize for phone

--- app/src/main/java/nl/bos/onepomodoroexercises/SettingsFragment.java
+++ app/src/main/java/nl/bos/onepomodoroexercises/SettingsFragment.java
@@ -3,6 +3,10 @@ package nl.bos.onepomodoroexercises;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 
 @SuppressWarnings("WeakerAccess")
@@ -16,6 +20,12 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
 
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        for(int i=0; i < prefScreen.getPreferenceCount(); i++) {
+            Preference pref = prefScreen.getPreference(i);
+            setColorPreferencesSummary(pref);
+        }
+
         Preference updateLocalData = findPreference(getString(R.string.updateLocalData));
         updateLocalData.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
@@ -24,6 +34,13 @@ public class SettingsFragment extends PreferenceFragment {
                 return true;
             }
         });
+    }
 
+    private void setColorPreferencesSummary(Preference pref) {
+        CharSequence cs = (CharSequence) pref.getSummary();
+        String plainTitle = cs.subSequence(0, cs.length()).toString();
+        Spannable coloredSummary = new SpannableString(plainTitle);
+        coloredSummary.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.rowBackgroundDone, null)), 0, coloredSummary.length(), 0);
+        pref.setSummary(coloredSummary);
     }
 }
